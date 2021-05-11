3510a286559cf1d526a7ead4fd652d1c59a08755
Celia <celionsebm3@gmail.com>
2021-05-11 14:32:53 +0200

    initial commit config ioc

---
+++ masb-pot-s-firmware/Core/Inc/Components/stm32main.h
@@ -0,0 +1,13 @@
+/*
+ * stm32main.h
+ *
+ *  Created on: May 11, 2021
+ *      Author: Celia
+ */
+
+#ifndef INC_COMPONENTS_STM32MAIN_H_
+#define INC_COMPONENTS_STM32MAIN_H_
+
+//llamamos a las features
+
+#endif /* INC_COMPONENTS_STM32MAIN_H_ */
